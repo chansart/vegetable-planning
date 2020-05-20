cfd50467ac9ecf00a8c269ca42d97d1c459c2310
charlottehansart <charlotte.hansart@bisnode.com>
2020-05-20 10:56:46 +0200

    Updates

---
+++ precache-manifest.2d14f161838a5529d24eb7cbbb4c198e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeed3a02492f0c8fec6fcd7744c153e1",
+    "url": "/vegetable-planning/index.html"
+  },
+  {
+    "revision": "711038caa72a7aec1f78",
+    "url": "/vegetable-planning/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a59ca6f31db10cf394c1",
+    "url": "/vegetable-planning/static/css/main.a5be1570.chunk.css"
+  },
+  {
+    "revision": "711038caa72a7aec1f78",
+    "url": "/vegetable-planning/static/js/2.7f48e3bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vegetable-planning/static/js/2.7f48e3bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59ca6f31db10cf394c1",
+    "url": "/vegetable-planning/static/js/main.111c153e.chunk.js"
+  },
+  {
+    "revision": "ce21b443570a77267d2e",
+    "url": "/vegetable-planning/static/js/runtime-main.667d3ceb.js"
+  }
+]);
